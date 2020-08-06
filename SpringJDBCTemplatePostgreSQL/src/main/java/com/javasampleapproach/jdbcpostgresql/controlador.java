34a0aa3d8395f89862d1a5e7694e9a49a0b5c403
ChristianBuele <christian97.b@hotmail.es>
2020-08-06 00:48:20 -0500

    ac users

--- SpringJDBCTemplatePostgreSQL/src/main/java/com/javasampleapproach/jdbcpostgresql/controlador.java
+++ SpringJDBCTemplatePostgreSQL/src/main/java/com/javasampleapproach/jdbcpostgresql/controlador.java
@@ -412,7 +412,7 @@ public ResponseEntity<String> getIdUsuario(@PathVariable("correo") String correo
 		try {
 			int id_usuario=Integer.parseInt(valores[0]);
 			int id_preset=Integer.parseInt(valores[1]);
-			boolean x=servtaricio.eliminarPresetCarrito(id_preset);
+			boolean x=servicio.eliminarPresetCarrito(id_preset);
 			if(x) {
 				usuario us=servicio.getDatosUsuario(id_usuario);
 				List<carritoDetallado> lista= servicio.getCarritoDetalladoPresets(us.getId_carrito());
